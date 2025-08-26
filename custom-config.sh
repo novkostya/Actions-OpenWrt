fe2f3e60d6d6ac50411b9da60f1b7671a973a749
Konstantin Novikov <novkostya@gmail.com>
2025-08-26 11:16:02 +0400

    Customization

--- custom-config.sh
+++ custom-config.sh
@@ -1,45 +1,24 @@
-# Use "make defconfig" to generate a complete .config file
+cat > "$BUILD_ROOT/.config" <<'EOF'
+CONFIG_TARGET_x86=y
+CONFIG_TARGET_x86_64=y
+CONFIG_TARGET_x86_64_DEVICE_generic=y
 
-# Custom firmware base
-# echo 'CONFIG_TARGET_x86=y' >> $BUILD_ROOT/.config
-# echo 'CONFIG_TARGET_x86_64=y' >> $BUILD_ROOT/.config
-# echo 'CONFIG_TARGET_x86_64_DEVICE_generic=y' >> $BUILD_ROOT/.config
-# echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=3072' >> $BUILD_ROOT/.config
+CONFIG_TARGET_ROOTFS_PARTSIZE=256
 
-echo 'CONFIG_TARGET_mediatek=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_mediatek_filogic=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_bananapi_bpi-r3-mini=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=4096' >> $BUILD_ROOT/.config
+CONFIG_PACKAGE_kmod-virtio=y
+CONFIG_PACKAGE_kmod-virtio-net=y
+CONFIG_PACKAGE_kmod-virtio-blk=y
+CONFIG_PACKAGE_kmod-virtio-scsi=y
+CONFIG_PACKAGE_kmod-virtio-ring=y
+CONFIG_PACKAGE_kmod-virtio-pci=y
+CONFIG_PACKAGE_pbr=y
+CONFIG_PACKAGE_zerotier=y
+CONFIG_PACKAGE_kmod-wireguard=y
+CONFIG_PACKAGE_wireguard-tools=y
+CONFIG_PACKAGE_kmod-amneziawg=y
+CONFIG_PACKAGE_amneziawg-tools=y
+CONFIG_PACKAGE_git=y
+CONFIG_PACKAGE_openssh-client=y
+CONFIG_PACKAGE_ca-bundle=y
 
-# Lanuage Setting
-echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> $BUILD_ROOT/.config
-
-# Users & Groups Support
-echo 'CONFIG_PACKAGE_shadow-utils=y' >> $BUILD_ROOT/.config
-
-# Disk Support
-echo 'CONFIG_PACKAGE_kmod-nvme=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_fdisk=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_block-mount=y' >> $BUILD_ROOT/.config
-
-# Add OpenClash
-echo '# CONFIG_PACKAGE_dnsmasq is not set' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILD_ROOT/.config
-
-# Add Docker
-echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILD_ROOT/.config
-
-# Add other apps
-echo 'CONFIG_PACKAGE_luci=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-ssl=y' >> $BUILD_ROOT/.config
-
-echo 'CONFIG_PACKAGE_luci-app-wol=y' >> $BUILD_ROOT/.config
-
-echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILD_ROOT/.config
-
-echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-upnp=y' >> $BUILD_ROOT/.config
+EOF
