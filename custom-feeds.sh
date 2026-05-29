7a364c9ae7fe58d1bb3a938859f126e26f8fef9a
Konstantin Novikov <novkostya@gmail.com>
2026-05-29 15:35:05 +0300

    Update amneziawg feed

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -1,1 +1,1 @@
-echo 'src-git amneziawg https://github.com/amnezia-vpn/amneziawg-openwrt.git' >> $BUILD_ROOT/feeds.conf.default
+echo 'src-git amneziawg https://github.com/Slava-Shchipunov/awg-openwrt.git' >> $BUILD_ROOT/feeds.conf.default
